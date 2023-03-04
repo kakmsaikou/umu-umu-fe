ff248601521dd36bc4ca0af9a3b679ba2d00d7f8
TacitusPie <kakmsaikou@gmail.com>
2023-03-04 09:43:14 +0800

    feat: 让背景图能适应插槽内容超出页面的情况

--- src/components/common/BackgroundWithCircle.tsx
+++ src/components/common/BackgroundWithCircle.tsx
@@ -1,11 +1,14 @@
 import { defineComponent } from 'vue';
 import s from './BackgroundWithCircle.module.scss';
+import NeroLogo from '../../assets/images/nero-logo.png'
+
 
 export const BackgroundWithCircle = defineComponent({
   setup: (props, context) => {
     return () => (
       <div class={s.wrapper}>
         <div class={s.bgCircle}></div>
+        <img class={s.neroLogo} src={NeroLogo} alt="" />
         {context.slots.default?.()}
       </div>
     );
--- src/views/LoginPage.tsx
+++ src/views/LoginPage.tsx
@@ -1,12 +1,10 @@
 import { defineComponent } from 'vue';
 import { BackgroundWithCircle } from '../components/common/BackgroundWithCircle';
 import s from './LoginPage.module.scss';
-import NeroLogo from '../assets/images/NeroLogo.png'
 
 export const LoginPage = defineComponent({
   setup: () => {
     return () => <BackgroundWithCircle>
-      <img class={s.neroLogo} src={NeroLogo} alt="" />
       <div>LoginPage</div>
     </BackgroundWithCircle>
   },
