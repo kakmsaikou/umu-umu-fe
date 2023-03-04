4036142a65adc02caddee8361d1741de70d61777
TacitusPie <kakmsaikou@gmail.com>
2023-03-04 13:01:35 +0800

    feat: BackgroundWithCircle 允许 props.neroText

--- src/components/common/BackgroundWithCircle.tsx
+++ src/components/common/BackgroundWithCircle.tsx
@@ -3,12 +3,18 @@ import s from './BackgroundWithCircle.module.scss';
 import { NeroLogoWithText } from './NeroLogoWithText';
 
 export const BackgroundWithCircle = defineComponent({
+  props: {
+    neroText: String,
+  },
   setup: (props, context) => {
     return () => (
       <div class={s.wrapper}>
         <div class={s.bgCircle} />
         <div class={s.mainContent}>
-          <NeroLogoWithText class={s.NeroLogoWithText} />
+          <NeroLogoWithText
+            class={s.NeroLogoWithText}
+            neroText={props.neroText}
+          />
           {context.slots.default?.()}
         </div>
       </div>
