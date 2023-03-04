61164caa7e0353809d214ab322147aae9f614006
TacitusPie <kakmsaikou@gmail.com>
2023-03-04 09:12:51 +0800

    feat: 引入尼禄 Logo/把 translate 改为 translateX

--- src/views/LoginPage.tsx
+++ src/views/LoginPage.tsx
@@ -1,10 +1,12 @@
 import { defineComponent } from 'vue';
-import { BackgroundWithCircle } from '../components/commen/BackgroundWithCircle';
+import { BackgroundWithCircle } from '../components/common/BackgroundWithCircle';
 import s from './LoginPage.module.scss';
+import NeroLogo from '../assets/images/NeroLogo.png'
 
 export const LoginPage = defineComponent({
   setup: () => {
     return () => <BackgroundWithCircle>
+      <img class={s.neroLogo} src={NeroLogo} alt="" />
       <div>LoginPage</div>
     </BackgroundWithCircle>
   },
