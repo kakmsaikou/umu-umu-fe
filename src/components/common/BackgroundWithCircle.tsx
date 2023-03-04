64de234f611560357385bcb625955bab7cf58544
TacitusPie <kakmsaikou@gmail.com>
2023-03-04 11:21:19 +0800

    feat: 支持在尼禄 logo 下插入文字了

--- src/components/common/BackgroundWithCircle.tsx
+++ src/components/common/BackgroundWithCircle.tsx
@@ -1,14 +1,22 @@
-import { defineComponent } from 'vue';
+import { defineComponent, PropType } from 'vue';
 import s from './BackgroundWithCircle.module.scss';
-import NeroLogo from '../../assets/images/nero-logo.png'
-
+import NeroLogo from '../../assets/images/nero-logo.png';
 
 export const BackgroundWithCircle = defineComponent({
+  props: {
+    neroText: {
+      type: String,
+      default: '　うむうむ！',
+    },
+  },
   setup: (props, context) => {
     return () => (
       <div class={s.wrapper}>
-        <div class={s.bgCircle}></div>
-        <img class={s.neroLogo} src={NeroLogo} alt="" />
+        <div class={s.bgCircle}/>
+        <div class={s.neroWrapper}>
+          <img class={s.neroLogo} src={NeroLogo} alt="" />
+          <div class={s.neroText}>{props.neroText}</div>
+        </div>
         {context.slots.default?.()}
       </div>
     );
