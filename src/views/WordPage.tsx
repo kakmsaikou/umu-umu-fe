437aa4c82199001fac84bcb92dd91c0c5012ca39
TacitusPie <kakmsaikou@gmail.com>
2023-03-06 00:50:41 +0800

    feat: 基本完成 WordPage CSS

--- src/views/WordPage.tsx
+++ src/views/WordPage.tsx
@@ -1,4 +1,5 @@
 import { defineComponent, reactive } from 'vue';
+import { SvgIcon } from '../components/common/SvgIcon';
 import s from './WordPage.module.scss';
 
 export const WordPage = defineComponent({
@@ -26,34 +27,64 @@ export const WordPage = defineComponent({
     });
     return () => (
       <div class={s.wrapper}>
-        <div>
+        <SvgIcon class={s.menuIcon} name='menu' />
+        <div class={s.wordWrapper}>
           <h1 class={s.word}>{wordData.word}</h1>
-          <p class={s.pronunciation}>{wordData.pronunciation}</p>
+          <div class={s.pronunciation}>
+            {wordData.pronunciation}
+            <SvgIcon class={s.voiceIcon} name='voice' />
+          </div>
         </div>
-        <div>
+
+        <div class={s.detailWrapper}>
           <div class={s.meaning}>
+            <h2>释义</h2>
             <p class={s.partOfSpeech}>{wordData.part_of_speech}</p>
             <p>{wordData.meaning}</p>
           </div>
-          <ul class={s.examples}>
-            {wordData.examples.map(example => (
-              <li>
-                <p>{example.example}</p>
-                <p>{example.translation}</p>
-              </li>
-            ))}
-          </ul>
+
+          <div class={s.examples}>
+            <h2>例句</h2>
+            <ul class={s.examplesList}>
+              {wordData.examples.map(item => (
+                <li>
+                  <p>{item.example}</p>
+                  <p class={s.translation}>{item.translation}</p>
+                </li>
+              ))}
+            </ul>
+          </div>
         </div>
-        <ul>
-          <li>认识</li>
-          <li>不确定</li>
-          <li>不认识</li>
+
+        <ul class={s.buttonsWrapper}>
+          <li>
+            <button>认识</button>
+          </li>
+          <li>
+            <button>不确定</button>
+          </li>
+          <li>
+            <button>不认识</button>
+          </li>
         </ul>
+
         <ul class={s.footerBar}>
-          <li>复习</li>
-          <li>选词</li>
-          <li>统计</li>
-          <li>设置</li>
+          <li class={s.selected}>
+            <SvgIcon class={s.svgIcon} name='time' />
+            <button>复习</button>
+          </li>
+          <li>
+            <SvgIcon class={s.svgIcon} name='book' />
+            <button>选词</button>
+          </li>
+          <li>
+            <SvgIcon class={s.svgIcon} name='chart' />
+            <button>统计</button>
+          </li>
+          <li>
+            <SvgIcon class={s.svgIcon} name='gear' />
+            <button>设置</button>
+          </li>
         </ul>
       </div>
     );
